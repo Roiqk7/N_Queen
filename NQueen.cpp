f5d0ab10617f107d010790886a89ac5556a931e5
Roiqk7 <112021405+Roiqk7@users.noreply.github.com>
2022-11-15 15:06:06 +0100

    Add files via upload

--- NQueen.cpp
+++ NQueen.cpp
@@ -1,60 +1,184 @@
-//  solves N queen using backtracking algorithm
-//  places Queen on every tile are returns average per position and total time
+// * g++ -std=c++17 NQueen.cpp -o NQueen -I/Users/roiqk/SFML/include -L/Users/roiqk/SFML/build/lib  -lsfml-graphics -lsfml-window -lsfml-system
 
+
+
+//  this program visualizes backtracking algorithm solving N Queens problem
+
+#include <array>        
 #include <iostream>
-#include <string.h>
+#include <random>
+#include <SFML/Graphics.hpp>
+
+//  map related -> base N = 8; TILE_SIZE = 100 
+//eg if you want to increase N from 8 to 16 you have to decrease TILE_SIZE from 100 to 50
+#define N 8                    //  size of one side of the square map
+#define TILE_SIZE 100          //  size of one tile on the map
+
+//  window related
+#define WIDTH N*TILE_SIZE    //  window width
+#define HEIGHT N*TILE_SIZE   //  window height
+#define QUEEN_SCALE 0.13     //  scale used for queen to fit into one tile
 
 
-#define N 18        //  size of the board
+// return values
+#define SUCCESS 0
+#define ERROR -1
 
-//  _NUM are shown only to program and _CHAR is shown to the user
+// create the window     
+sf::RenderWindow window(sf::VideoMode(sf::Vector2u(WIDTH, HEIGHT)), "Backtracking algorithm visualizer");
+
+//  constant values describing board
 const int QUEEN_NUM = 1;
 const int EMPTY_NUM = 0;
-const char QUEEN_CHAR = 'Q';
-const char EMPTY_CHAR = '.';
 
 
-int NQmain();                                       //  directs the program
-bool isValid(int row, int col, int board[N][N]);    //  helper function for NQsolve 
-bool NQsolve(int row, int board[N][N]);             //  solves NQ using backtracking algorithm
-void printOut(int board[N][N]);                     //  prints board
+/*
+     ######  ##          ###     ######   ######  
+    ##    ## ##         ## ##   ##    ## ##    ## 
+    ##       ##        ##   ##  ##       ##       
+    ##       ##       ##     ##  ######   ######  
+    ##       ##       #########       ##       ## 
+    ##    ## ##       ##     ## ##    ## ##    ## 
+     ######  ######## ##     ##  ######   ######  
+*/
 
 
-int main(void) 
-{
-    NQmain();
-}
+//  Board stores all important data and renders the actual board seen by the user
+//  also draws queens
+class Board {
+    public:
+        std::array<sf::RectangleShape, N> boardTiles;
+        std::array<std::array<int, N>, N> map;
+        sf::Texture whiteQueenTexture;
+        sf::Texture blackQueenTexture;
+        sf::Sprite queenSprite;
+        int black;        
 
-
-int NQmain() 
-{
-    int board[N][N];
-    long double timeSum;
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            memset(board, EMPTY_NUM, sizeof(board));
-            board[i][j] = QUEEN_NUM;
-            NQsolve(0, board);
-            printOut(board);
+        Board() {
+            map = {{EMPTY_NUM}};
+            black = 0;
+            if (!whiteQueenTexture.loadFromFile("/Users/roiqk/Desktop/Programming/C:C++/C++/NumeroDuo/Assets/whiteQueen.png")) std::exit(0);
+            if (!blackQueenTexture.loadFromFile("/Users/roiqk/Desktop/Programming/C:C++/C++/NumeroDuo/Assets/blackQueen.png")) std::exit(0);
         }
+
+        //  draws the bard
+        void render(void)
+        {
+            window.clear();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    boardTiles[i].setPosition (sf::Vector2f(j*TILE_SIZE, i*TILE_SIZE));
+                    boardTiles[i].setSize(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+                    queenSprite.setScale(sf::Vector2f(QUEEN_SCALE, QUEEN_SCALE));
+                    if (black % 2 == true) {
+                        boardTiles[i].setFillColor(sf::Color::Black);
+                        queenSprite.setTexture(whiteQueenTexture);
+                    } else {
+                        boardTiles[i].setFillColor(sf::Color::White);
+                        queenSprite.setTexture(blackQueenTexture);
+                    }
+                    window.draw(boardTiles[i]);
+                    if (map[i][j] == QUEEN_NUM) {
+                        queenSprite.setPosition (sf::Vector2f(j*TILE_SIZE, i*TILE_SIZE));
+                        window.draw(queenSprite);
+                    }
+                    black++; 
+                }
+                black++;
+            }
+            window.display();
+        }
+};
+
+
+void addRandomQueen(Board &board);              //  adds random queen for uniqueness
+void sfml(Board &board);                        //  draws the screen
+bool NQsolve(Board &board, int row);            //  backtracking algorithm solving N Queen
+void printOut(Board &board);                    //  prints board to the console
+
+
+/*
+    ##     ##    ###    #### ##    ## 
+    ###   ###   ## ##    ##  ###   ## 
+    #### ####  ##   ##   ##  ####  ## 
+    ## ### ## ##     ##  ##  ## ## ## 
+    ##     ## #########  ##  ##  #### 
+    ##     ## ##     ##  ##  ##   ### 
+    ##     ## ##     ## #### ##    ## 
+*/
+
+
+//  starts the program
+int main()
+{
+    while (true) {
+        Board board = Board();
+
+        addRandomQueen(board);
+
+        sfml(board);
+
     }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    timeSum += duration.count();
-    std::cout << std::endl << "Average time per position is " << timeSum/(N*N) << "s and total is " << timeSum << 's';
-    return 0;
+    return SUCCESS;
 }
+
+
+//  makes the program more unique and interesting
+void addRandomQueen(Board &board)
+{
+    srand(time(NULL));
+    int row = rand()%N;
+    int col = rand()%N;
+    board.map[row][col] = QUEEN_NUM;
+}
+
+
+/*
+     ######  ######## ##     ## ##       
+    ##    ## ##       ###   ### ##       
+    ##       ##       #### #### ##       
+     ######  ######   ## ### ## ##       
+          ## ##       ##     ## ##       
+    ##    ## ##       ##     ## ##       
+     ######  ##       ##     ## ######## 
+*/
+
+
+//  visualizes the algorithm
+void sfml(Board &board)
+{
+    window.setFramerateLimit (60);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        NQsolve(board, 0);
+    }
+    return;
+}
+
+
+/*
+       ###    ##        ######    #######  ########  #### ######## ##     ## ##     ## 
+      ## ##   ##       ##    ##  ##     ## ##     ##  ##     ##    ##     ## ###   ###
+     ##   ##  ##       ##        ##     ## ##     ##  ##     ##    ##     ## #### #### 
+    ##     ## ##       ##   #### ##     ## ########   ##     ##    ######### ## ### ## 
+    ######### ##       ##    ##  ##     ## ##   ##    ##     ##    ##     ## ##     ##     
+    ##     ## ##       ##    ##  ##     ## ##    ##   ##     ##    ##     ## ##     ##
+    ##     ## ########  ######    #######  ##     ## ####    ##    ##     ## ##     ##  
+*/
 
 
 //  checks if queen can be placed on specific position on the board
-bool isValid(int row, int col, int board[N][N])
+bool isValid(Board &board, int &row, int &col)
 {
-    for (int i = 0; i < N; i++) if (board[i][col] == QUEEN_NUM) return false;
+    for (int i = 0; i < N; i++) if (board.map[i][col] == QUEEN_NUM) return false;
     for (int i = 0; i < N; i++) { 
         for (int j = 0; j < N; j++) {
             if (i + j == row + col || i - j == row - col) {
-                if (board[i][j] == QUEEN_NUM) return false;
+                if (board.map[i][j] == QUEEN_NUM) return false;
             }
         }
     }
@@ -62,24 +186,33 @@ bool isValid(int row, int col, int board[N][N])
 }
 
 
-//  time complexity O(n!)
-bool NQsolve(int row, int board[N][N]) 
+/*
+* Implementation of backtracking algorithm, O(n!)
+The idea is to place queens one by one in different columns, 
+starting from the leftmost column. When we place a queen in a column, 
+we check for clashes with already placed queens. In the current column, 
+if we find a row for which there is no clash, we mark this row and column as part of the solution. 
+If we do not find such a row due to clashes, then we backtrack and return false.
+*/
+bool NQsolve(Board &board, int row) 
 {
     if (row == N) return true;
-    for (int i = 0; i < N; i++) if (board[row][i] == QUEEN_NUM) return NQsolve(row+1, board);
+    for (int i = 0; i < N; i++) if (board.map[row][i] == QUEEN_NUM) return NQsolve(board, row+1);
     for (int i = 0; i < N; i++) {
-        if (isValid(row, i, board)) {
-            board[row][i] = QUEEN_NUM;
-            if (NQsolve(row+1, board)) return true;
+        if (isValid(board, row, i)) {
+            board.map[row][i] = QUEEN_NUM;
+            board.render();
+            printOut(board);
+            if (NQsolve(board, row+1)) return true;
         }
-        board[row][i] = EMPTY_NUM;
+        board.map[row][i] = EMPTY_NUM;
     }
     return false;
 }
 
 
-//  print the board in user-readable form and returns Queen positions
-void printOut(int board[N][N])
+//  print the board in user-readable form and returns Queen positions (not designed for N > 27)
+void printOut(Board &board)
 {
     std::cout << std::endl;
     std::string formattedReturn;
@@ -87,15 +220,15 @@ void printOut(int board[N][N])
     std::cout << std::endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (board[i][j] == QUEEN_NUM) {
-                std::cout << ' ' << QUEEN_CHAR;
+            if (board.map[i][j] == QUEEN_NUM) {
+                std::cout << ' ' << 'Q';
                 formattedReturn += 'a' + i%26;
                 formattedReturn += std::to_string(N - j);
                 if (i != N - 1 && j != N - 1) formattedReturn += ", ";
             }
-            else std::cout << ' ' << EMPTY_CHAR;
+            else std::cout << ' ' << '.';
         }
-        std::cout << '|' << N - i << std::endl;
+        std::cout << '|' << N - i << '\n';
     }
-    std::cout << formattedReturn << std::endl;
+    std::cout << '\n' << formattedReturn << '\n';
 }
